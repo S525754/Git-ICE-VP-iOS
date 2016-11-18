c25b6efcf856fad6c00c4ed34bd1b418ef2ab459
Student <student@nw36640.nwmissouri.edu>
2016-11-18 13:10:28 -0600

    Add Comment

--- Git-ICE-VP/EhVC.swift
+++ Git-ICE-VP/EhVC.swift
@@ -12,6 +12,7 @@ class EhVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Vinod ga push chesina chusko")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
